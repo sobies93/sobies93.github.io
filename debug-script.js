f0971941a9b44ffe3cf3b62394697fae5ed1a35a
Szymon Sobiepanek <ssobiepanek@gmail.com>
2024-07-31 09:07:04 +0200

    check winstone logging

--- debug-script.js
+++ debug-script.js
@@ -1,5 +1,20 @@
+const { createLogger, format, transports } = require("winston");
+const { printf } = format;
+
+const customFormat = printf(({ level, message, timestamp }) => {
+  return `${message}`;
+});
+
+const logger = createLogger({
+  level: "debug",
+  format: customFormat,
+  transports: [new transports.Console()],
+});
+
+module.exports = logger;
+
 const { argv } = require("node:process");
 
 const [, , client] = argv;
 
-console.log(client);
+logger.debug(client);
